ad8776f82ff625de55f1f1f194ffae2534a0aafc
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2025-03-27 20:25:21 +0530

    Working with Union Types

--- packages/notes-ts-fundamentals-v4/src/06-union-and-intersection-types.ts
+++ packages/notes-ts-fundamentals-v4/src/06-union-and-intersection-types.ts
@@ -20,52 +20,53 @@ let evenOrLowNumber = 5 as Evens | OneThroughFive;
 
 
 //? Control flow sometimes results in union types
-// function flipCoin() {
-//     if (Math.random() > 0.5) return "heads"
-//     return "tails"
-// }
+function flipCoin(): "heads" | "tails" {
+    if (Math.random() > 0.5) return "heads"
+    return "tails" as const
+}
 
-// const outcome = flipCoin()
+const outcome = flipCoin()
 // //     ^? "heads" | "tails"
 // //? A more complicated example
 
-// const success = ["success", { name: "Mike North", email: "mike@example.com" }] as const
-// const fail = ["error", new Error("Something went wrong!")] as const
+const success = ["success", { name: "Mike North", email: "mike@example.com" }] as const
+const fail = ["error", new Error("Something went wrong!")] as const
 
-/*
-// function maybeGetUserInfo() {
-//     if (flipCoin() === "heads") {
-//         return success
-//     } else {
-//         return fail
-//     }
-// }
 
-// const outcome2 = maybeGetUserInfo()
+function maybeGetUserInfo() {
+    if (flipCoin() === "heads") {
+        return success
+    } else {
+        return fail
+    }
+}
+
+const outcome2 = maybeGetUserInfo()
+// outcome2[1].
 
 //* Working with union types
-/*
+
 //? Think critically: "AND" vs "OR", as it pertains to the contents of the set,
 //? vs the assumptions we can make about the value
-// function printEven(even: Evens): void { }
-// function printLowNumber(lowNum: OneThroughFive): void { }
-// function printEvenNumberUnder5(num: 2 | 4): void { }
-// function printNumber(num: number): void { }
+function printEven(even: Evens): void { }
+function printLowNumber(lowNum: OneThroughFive): void { }
+function printEvenNumberUnder5(num: 2 | 4): void { }
+function printNumber(num: number): void { }
 
-// let x = 5 as Evens | OneThroughFive;
+let x = 5 as Evens | OneThroughFive;
 
-/*
+
 //? What does Evens | OneThroughFive accept as values?
 // let evenOrLowNumber: Evens | OneThroughFive;
-// evenOrLowNumber = 6 //✔️ An even
-// evenOrLowNumber = 3 //✔️ A low number
-// evenOrLowNumber = 4 //✔️ A even low number
+evenOrLowNumber = 6 //✔️ An even
+evenOrLowNumber = 3 //✔️ A low number
+evenOrLowNumber = 4 //✔️ A even low number
 
 //? What requirements can `Evens | OneThroughFive` meet?
-// printEven(x) //! Not guaranteed to be even
-// printLowNumber(x) //! Not guaranteed to be in {1, 2, 3, 4, 5}
-// printEvenNumberUnder5(x) //! Not guaranteed to be in {2, 4}
-// printNumber(x) //✔️ Guaranteed to be a number
+printEven(x) //! Not guaranteed to be even
+printLowNumber(x) //! Not guaranteed to be in {1, 2, 3, 4, 5}
+printEvenNumberUnder5(x) //! Not guaranteed to be in {2, 4}
+printNumber(x) //✔️ Guaranteed to be a number
 
 //* Narrowing with type guards
 /*
