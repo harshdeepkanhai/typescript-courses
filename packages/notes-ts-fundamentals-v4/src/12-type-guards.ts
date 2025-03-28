02b6002b14956953042abc48c918fa10038d104c
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2025-03-28 14:53:41 +0530

    User Defined Type Guard (value is Foo)

--- packages/notes-ts-fundamentals-v4/src/12-type-guards.ts
+++ packages/notes-ts-fundamentals-v4/src/12-type-guards.ts
@@ -41,52 +41,52 @@ else if ('dateRange' in value) {
   // ^?
 }
 //* User-defined type guards
-/*
-// interface CarLike {
-//     make: string
-//     model: string
-//     year: number
-// }
 
-// let maybeCar: any
+interface CarLike {
+    make: string
+    model: string
+    year: number
+}
 
-// // the guard
-// if (
-//     maybeCar &&
-//     typeof maybeCar === "object" &&
-//     "make" in maybeCar &&
-//     typeof maybeCar["make"] === "string" &&
-//     "model" in maybeCar &&
-//     typeof maybeCar["model"] === "string" &&
-//     "year" in maybeCar &&
-//     typeof maybeCar["year"] === "number"
-// ) {
-//     maybeCar
-//     // ^?
-// }
-/*
-// // the guard
-// function isCarLike(valueToTest: any) {
-//     return (
-//         valueToTest &&
-//         typeof valueToTest === "object" &&
-//         "make" in valueToTest &&
-//         typeof valueToTest["make"] === "string" &&
-//         "model" in valueToTest &&
-//         typeof valueToTest["model"] === "string" &&
-//         "year" in valueToTest &&
-//         typeof valueToTest["year"] === "number"
-//     )
-// }
+let maybeCar: any
 
-// // using the guard
-// if (isCarLike(maybeCar)) {
-//     maybeCar
-//     // ^?
-// }
+// the guard
+if (
+    maybeCar &&
+    typeof maybeCar === "object" &&
+    "make" in maybeCar &&
+    typeof maybeCar["make"] === "string" &&
+    "model" in maybeCar &&
+    typeof maybeCar["model"] === "string" &&
+    "year" in maybeCar &&
+    typeof maybeCar["year"] === "number"
+) {
+    maybeCar
+    // ^?
+}
+
+// the guard
+function isCarLike(valueToTest: any): valueToTest is CarLike {
+    return (
+        valueToTest &&
+        typeof valueToTest === "object" &&
+        "make" in valueToTest &&
+        typeof valueToTest["make"] === "string" &&
+        "model" in valueToTest &&
+        typeof valueToTest["model"] === "string" &&
+        "year" in valueToTest &&
+        typeof valueToTest["year"] === "number"
+    )
+}
+
+// using the guard
+if (isCarLike(maybeCar)) {
+    maybeCar
+    // ^?
+}
 
 //* value is foo
-/*
+
 // function isCarLike(valueToTest: any): valueToTest is CarLike {
 
 //* asserts value is foo
