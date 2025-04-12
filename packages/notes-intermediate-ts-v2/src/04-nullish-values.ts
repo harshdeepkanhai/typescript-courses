544ba00422b9d4bf13665cc8faf1e5974797a142
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2025-04-13 00:42:14 +0530

    With Nullish Coalescing

--- packages/notes-intermediate-ts-v2/src/04-nullish-values.ts
+++ packages/notes-intermediate-ts-v2/src/04-nullish-values.ts
@@ -109,8 +109,7 @@ type PlayerConfig = {
 }
 
 function initializePlayer(config: PlayerConfig): void {
-  const vol =
-    typeof config.volume === 'undefined' ? 50 : config.volume
+  const vol = config.volume ?? 50
   setVolume(vol)
 }
 
