efea5e3458783ad727863240de344fde4cbcdff6
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2025-04-13 01:37:53 +0530

    Scopes and TypeParams

--- packages/notes-intermediate-ts-v2/src/06-generic-scopes-and-constraints.ts
+++ packages/notes-intermediate-ts-v2/src/06-generic-scopes-and-constraints.ts
@@ -80,27 +80,27 @@ testResult.a.
 // function listToDict<T extends HasId>(list: T[]): Dict<T> {
 
 //* Scopes and Type Parameters
-/*
-// function eatApple(bowl: any, eater: (arg: any) => void) {}
 
-// function receiveFruitBasket(bowl: any) {
-//   console.log('Thanks for the fruit basket!')
-//   // only `bowl` can be accessed here
-//   eatApple(bowl, (apple: any) => {
-//     // both `bowl` and `apple` can be accessed here
-//   })
-// }
+function eatApple(bowl: any, eater: (arg: any) => void) {}
 
-// // outer function
-// function tupleCreator<T>(first: T) {
-//   // inner function
-//   return function finish<S>(last: S): [T, S] {
-//     return [first, last]
-//   }
-// }
-// const finishTuple = tupleCreator(3 as const)
-// const t1 = finishTuple(null)
-// const t2 = finishTuple([4, 8, 15, 16, 23, 42])
+function receiveFruitBasket(bowl: any) {
+  console.log('Thanks for the fruit basket!')
+  // only `bowl` can be accessed here
+  eatApple(bowl, (apple: any) => {
+    // both `bowl` and `apple` can be accessed here
+  })
+}
+
+// outer function
+function tupleCreator<T>(first: T) {
+  // inner function
+  return function finish<S>(last: S): [T, S] {
+    return [first, last]
+  }
+}
+const finishTuple = tupleCreator(3 as const)
+const t1 = finishTuple(null)
+const t2 = finishTuple([4, 8, 15, 16, 23, 42])
 
 //* Best practices
 // interface HasId {
@@ -110,37 +110,37 @@ testResult.a.
 //   [k: string]: T
 // }
 
-// function example1<T extends HasId[]>(list: T) {
-//   return list.pop()
-//   //      ^?
-// }
-// function example2<T extends HasId>(list: T[]) {
-//   return list.pop()
-//   //      ^?
-// }
+function example1<T extends HasId[]>(list: T) {
+  return list.pop()
+  //      ^?
+}
+function example2<T extends HasId>(list: T[]) {
+  return list.pop()
+  //      ^?
+}
 
-// class Payment implements HasId {
-//   static #next_id_counter = 1;
-//   id = `pmnt_${Payment.#next_id_counter++}`
-// }
-// class Invoice implements HasId {
-//   static #next_id_counter = 1;
-//   id = `invc_${Invoice.#next_id_counter++}`
-// }
+class Payment implements HasId {
+  static #next_id_counter = 1;
+  id = `pmnt_${Payment.#next_id_counter++}`
+}
+class Invoice implements HasId {
+  static #next_id_counter = 1;
+  id = `invc_${Invoice.#next_id_counter++}`
+}
 
-// const result1 = example1([
-//   //   ^?
-//   new Payment(),
-//   new Invoice(),
-//   new Payment()
-// ])
+const result1 = example1([
+  //   ^?
+  new Payment(),
+  new Invoice(),
+  new Payment()
+])
 
-// const result2 = example2([
-//   //   ^?
-//   new Payment(),
-//   new Invoice(),
-//   new Payment()
-// ])
+const result2 = example2([
+  //   ^?
+  new Payment(),
+  new Invoice(),
+  new Payment()
+])
 
 /**/
 export default {}
