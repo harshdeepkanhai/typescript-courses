d7b111f930ff6370ffc8ed45874f034befd19dac
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2025-04-13 22:45:13 +0530

    Add Extract and Exclude Utility Types

--- packages/notes-intermediate-ts-v2/src/07-conditional-types.ts
+++ packages/notes-intermediate-ts-v2/src/07-conditional-types.ts
@@ -31,37 +31,37 @@ type TestTen = IsLowNumber<10>
 type TestTenWithTwo = IsLowNumber<10 | 2>
 
 //* Extract<T, U>
-/*
-// type FavoriteColors =
-//   | 'dark sienna'
-//   | 'van dyke brown'
-//   | 'yellow ochre'
-//   | 'sap green'
-//   | 'titanium white'
-//   | 'phthalo green'
-//   | 'prussian blue'
-//   | 'cadium yellow'
-//   | [number, number, number]
-//   | { red: number; green: number; blue: number }
 
-// type StringColors = Extract<FavoriteColors, string>
-// //    ^?
-// type ObjectColors = Extract<FavoriteColors, { red: number }>
-// //    ^?
-// type TupleColors =
-//   //     ^?
-//   Extract<FavoriteColors, [number, number, number]>
+type FavoriteColors =
+  | 'dark sienna'
+  | 'van dyke brown'
+  | 'yellow ochre'
+  | 'sap green'
+  | 'titanium white'
+  | 'phthalo green'
+  | 'prussian blue'
+  | 'cadium yellow'
+  | [number, number, number]
+  | { red: number; green: number; blue: number }
+
+type StringColors = Extract<FavoriteColors, string>
+//    ^?
+type ObjectColors = Extract<FavoriteColors, { red: number }>
+//    ^?
+type TupleColors =
+  //     ^?
+  Extract<FavoriteColors, [number, number, number]>
 
 //* Exclude<T, U>
-/*
-// type NonStringColors = Exclude<FavoriteColors, string>
+
+type NonStringColors = Exclude<FavoriteColors, string>
 
 //* How do these work?
-/*
-// type _Exclude<T, U> = T extends U ? never : T
-// type _Extract<T, U> = T extends U ? T : never
 
-// type OneNever = 1 | never
+type _Exclude<T, U> = T extends U ? never : T
+type _Extract<T, U> = T extends U ? T : never
+
+type OneNever = 1 | never
 
 /**/
 
